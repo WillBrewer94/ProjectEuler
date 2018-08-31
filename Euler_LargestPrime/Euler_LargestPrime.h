4ff731bcf28170d3132c955429ad457cf720804d
Will Brewer <willbrewer182@gmail.com>
2018-08-31 17:35:59 -0400

    Initial Commit

---
+++ Euler_LargestPrime/Euler_LargestPrime.h
@@ -0,0 +1,1 @@
+unsigned long long largest_prime(unsigned long long);
--- ProjectEuler/EvenFib.cpp
+++ ProjectEuler/EvenFib.cpp
@@ -1,3 +1,8 @@
+// EvenFib.cpp - By Will Brewer
+// Sum of Even Fibonacci Sequence - Project Euler #2
+//
+// By considering the terms in the Fibonacci sequence whose values do not exceed four million, 
+// find the sum of the even-valued terms.
 #include "EvenFib.h"
 #include <iostream>
 
@@ -7,12 +12,32 @@ int main(int argc, char* argv)
 	return 0;
 }
 
+// Generates the sum of all even numbers in fibonacci sequence of lim
+// Param: int
+// Return: int
 int fib_sum(int lim)
 {
+	int fib = 1;
+	int lastFib = 1;
+	int temp = 0;
+	int sum = 0;
+
 	if (lim < 2)
 	{
 		return lim;
 	}
 
-	return fib_sum(lim - 1) + fib_sum(lim - 2);
+	while (fib < lim)
+	{
+		if (fib % 2 == 0)
+		{
+			sum += fib;
+		}
+
+		temp = fib;
+		fib += lastFib;
+		lastFib = temp;
+	}
+
+	return sum;
 }
